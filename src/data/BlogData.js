c9903b49013ad114dfe5a66440cc52918b1facd6
Saritagomi <saritasbikhatod@gmail.com>
2024-04-24 21:42:28 +0530

    inital commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,71 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Chandigarh University | MOHALI, PUNJAB | Master’s Degree in Computer",
+        tags:["chandigarh university"],
+        date:"Python developer, DSA",
+        imgSrc:"https://img.jagranjosh.com/images/2022/March/1132022/Screenshot%202022-03-09%20112654.png",
+        link:"https://www.cuchd.in/"
+    },
+    {
+        id:2,
+        name:"University of Delhi |B.sc Mathematics honors",
+        tags:["University of Delhi"],
+        date:"Received the Outstanding Class Representative award for exceptional leadership to the student community",
+        imgSrc:"https://static.theprint.in/wp-content/uploads/2021/01/artsfac-696x392.jpg?compress=true&quality=80&w=768&dpr=1.8",
+        link:"https://www.du.ac.in/"
+    },
+    {
+        id:3,
+        name:"B S Sr Sec School | MAHENDRAGARH, HARYANA |Intermediate Non-Medical",
+        tags:["HBSE"],
+        date:"Majored in Mathematics Achieved a remarkable score of 94 in the mathematics subject, emerging as the highest achiever in my class",
+        imgSrc:"https://www.warpaintjournal.com/wp-content/uploads/2020/08/Larkmead_School_Abingdon_Oxfordshire.png",
+        link:"https://bseh.org.in/homes"
+    },
+    {
+        id:4,
+        name:"National Model School | Mahendragarh, Haryana High School",
+        tags:["HBSE"],
+        date:"Consistently maintained a position in the top percentile, achieving a score of 95 in mathematics throughout my high school years.",
+        imgSrc:"https://www.warpaintjournal.com/wp-content/uploads/2020/08/Larkmead_School_Abingdon_Oxfordshire.png",
+        link:"https://bseh.org.in/home"
+    },
+    {
+        id:5,
+        name:"React JS- Infosys Springboard",
+        tags:["react","reactJS","web-development"],
+        date:"23rd November 2022",
+        imgSrc:"https://www.infosys.com/content/dam/infosys-web/en/global-resource/18/springboard-logo.png",
+        link:"https://www.infosys.com/about/springboard.html"
+    },
+    {
+        id:6,
+        name:"Node JS- Infosys Springboard",
+        tags:["NODE.JS"],
+        date:"23rd November 2022",
+        imgSrc:"https://www.infosys.com/content/dam/infosys-web/en/global-resource/18/springboard-logo.png",
+        link:"https://www.infosys.com/about/springboard.html"
+
+    },
+   
+    {
+        id:7,
+        name:"R Studio, Geogebra",
+        tags:["IIT Bombay"],
+        date:"16 May, 2021",
+        imgSrc:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/IITBMainBuildingCROP.jpg/450px-IITBMainBuildingCROP.jpg",
+        link:"https://www.iitb.ac.in/"
+    },
+    {
+        id:8,
+        name:"Advanced Business Analytics ",
+        tags:["Coursera"],
+        date:" September 2023",
+        imgSrc:"https://facts.net/wp-content/uploads/2023/09/18-surprising-facts-about-coursera-1695863650.jpeg",
+        link:"https://www.coursera.org/courseraplus?utm_source=gg&utm_medium=sem&utm_campaign=B2C_INDIA_Branded_FTCOF_Coursera_Plus_Subscription_ArtE&utm_content=B2C&campaignid=20590309416&adgroupid=155702724684&device=c&keyword=coursera&matchtype=e&network=g&devicemodel=&adpostion=&creativeid=675426312952&hide_mobile_promo&gad_source=1&gclid=Cj0KCQjwlZixBhCoARIsAIC745CLKBP3EhQco5sm00G6i7WEuRXul1c4Vt4yI2amxQS4idwZYjWGMdIaAlvLEALw_wcB"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Sarita Portfolio",
+        description:"React JS-based portfolio featuring styledComponents and  sleek, smooth scrolling animations.",
+        tags:["react","node.js","styledComponents"],
+        
+        demo:"https://github.com/Saritagomi/",
+        github:"https://github.com/Saritagomi/sarita_portfolio"
+    },
+    {
+        id:2,
+        name:"Bank Website",
+        description:"This is a modern payment website which is made using Node.js, Tailwind CSS, JavaScript .",
+        tags:["react","HTML","CSS"],
+        
+        demo:"https://bank-website-lemon.vercel.app/",
+        github:"https://github.com/Saritagomi/bank_website"
+        
+    },
+    {
+        id:3,
+        name:"Travel website",
+        description:"Built with Next.js, TailwindCSS, and TypeScript, this landing page not only looks great but also serves as a hands-on project to reinforce one's understanding of these technologies.",
+        tags:["react","api"],
+        
+        demo:"https://travel-website-gamma-sable.vercel.app/",
+        github:"https://github.com/Saritagomi/travel_website"
+    },
+    {
+        id:4,
+        name:"Iphone website",
+        description:"This is a clone of Apple's iPhone 15 Pro website using React.js and TailwindCSS. It highlights the effective use of GSAP (Greensock Animations) and Three.js for displaying iPhone 15 Pro models in various colors and shapes.",
+        tags:["react","styledComponents"],
+        
+        demo:"https://iphone-website-beta.vercel.app/",
+        github:"https://github.com/Saritagomi/iphone_website"
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"http://react",
+        github:"https://github.com"
+        
+    },
+    {
+        id:6,
+        name:"Metaverse",
+        description:"This is a modern website created with React.js and framer motion and Tailwind CSS.",
+        tags:["react","framer-motion"],
+        
+        demo:"https://metaversus-two-sage.vercel.app/",
+        github:"https://github.com/Saritagomi/Metaversus"
+        
+    },
+    
+  
+] 
