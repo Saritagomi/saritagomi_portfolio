fe90c16405115627091b98e68602bc257177f579
Saritagomi <saritasbikhatod@gmail.com>
2024-04-24 23:07:24 +0530

    inital commit

--- src/subComponents/SoundBar.js
+++ src/subComponents/SoundBar.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import styled, { keyframes } from 'styled-components'
 
-import music from "C:/Users/hp/Desktop/github/saritagomi_portfolio/src/assets/audio/u-said-it-v13-1167.mp3"
+import music from "../assets/audio/u-said-it-v13-1167.mp3"
 
 const Box = styled.div`
 display:flex;
